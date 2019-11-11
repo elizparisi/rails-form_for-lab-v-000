40559920c3d198dd184fd53531f6fcf1259f6493
Liz P <parisielizabeth@gmail.com>
2019-11-11 21:45:23 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,9 +3,11 @@ class SchoolClassesController < ApplicationController
   def index 
     @school_classes = SchoolClass.all
   end
+  
   def show 
     @school_class = SchoolClass.find(params[:id])
   end
+  
   def new 
     @school_class = SchoolClass.new
   end 
