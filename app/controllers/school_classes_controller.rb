dd2b7404d966b1019942f6c80b945e68fc980b4c
Liz P <parisielizabeth@gmail.com>
2019-11-11 21:55:38 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -19,13 +19,14 @@ class SchoolClassesController < ApplicationController
   end 
   
    
-  
   def edit 
     @school_class = SchoolClass.find(params[:id])
   end 
   
   def update 
-    
+    @school_class = SchoolClass.find(params[:id])
+    @school_class = SchoolClass.update(school_class_params)
+    redirect_to school_class_path(@school_class)
   end 
   
   private
